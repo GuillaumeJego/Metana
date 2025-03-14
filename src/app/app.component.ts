24a3682beb6fce1b2635712434aa8b26b5e4b9e8
Diego <jego.guillaume22@gmail.com>
2025-03-14 23:41:07 +0100

    add symphony

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FooterComponent,
+    HeaderComponent,
+    HeroComponent,
+    ServicesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
