862a4de0edc80d28e8c4bf0da7ead76d2193a350
Diego <jego.guillaume22@gmail.com>
2025-03-16 18:44:38 +0100

    abandon symfony

---
+++ src/app/header/header-navbar/header-navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-navbar',
+  imports: [],
+  templateUrl: './header-navbar.component.html',
+  styleUrl: './header-navbar.component.scss'
+})
+export class HeaderNavbarComponent {
+
+  constructor() {}
+
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { LogoComponent } from './logo/logo.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    LoginComponent,
+    LogoComponent,
+    HeaderNavbarComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
